9fd13351fdd6f2f712c31a49d2c7752a12963285
BROWN-PROGRAMTECH <aldenovpoutine99@gmail.com>
2018-07-14 12:40:56 +0100

    exercise 1

---
+++ Brown/exe1.py
@@ -0,0 +1,6 @@
+2-Experiment by entering different expressions and recording what is displayed by the Python interpreter.
+             # using ubuntu terminal
+   print(2+5) #the interpreter return 7.
+   print(2**4) #the interpreter return 16.
+   print(4*5) #the interpreter return 20.
+   print(4*6) #the interpreter return 24.
