9ff79d236aecc8f3f249590c0a33f9c7230e31f4
BROWN-PROGRAMTECH <aldenovpoutine99@gmail.com>
2018-07-15 13:56:32 +0100

    exercise 1

--- Brown/exe1.py
+++ Brown/exe1.py
@@ -1,6 +1,7 @@
-2-Experiment by entering different expressions and recording what is displayed by the Python interpreter.
-             # using ubuntu terminal
-   print(2+5) #the interpreter return 7.
-   print(2**4) #the interpreter return 16.
-   print(4*5) #the interpreter return 20.
-   print(4*6) #the interpreter return 24.
+ 1-a) An English (or Dutch!) sentence with understandable semantics but incorrect syntax is
+  
+ i AM AN epc student,
+  
+ -b)English (or Dutch!) sentence which has correct syntax but has semantic errors is 
+
+ I python love program.
