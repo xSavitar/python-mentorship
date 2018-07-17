3659389521d2e19ecbf4adc66015fad87f9537fc
BROWN-PROGRAMTECH <aldenovpoutine99@gmail.com>
2018-07-17 13:14:11 +0100

    Chapter_1

--- Brown/Chapter_1/exercise_1.12.1.py
+++ Brown/Chapter_1/exercise_1.12.1.py
@@ -1,7 +1,7 @@
- 1-a) An English (or Dutch!) sentence with understandable semantics but incorrect syntax is
+ 1-a) sentence with understandable semantics but incorrect syntax is
   
  i AM AN epc student,
   
- -b)English (or Dutch!) sentence which has correct syntax but has semantic errors is 
+ -b) sentence which has correct syntax but has semantic errors is 
 
  I python love program.
