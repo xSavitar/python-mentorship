a657d2c8cf07228c9d7109523aebca1b80a181e2
BROWN-PROGRAMTECH <39649631+BROWN-PROGRAMTECH@users.noreply.github.com>
2018-07-20 19:41:45 +0100

    Delete exercise_1.12.1.py

--- Brown/Chapter_1/exercise_1.12.1.py
+++
@@ -1,7 +0,0 @@
- 1-a) sentence with understandable semantics but incorrect syntax is
-  
- i AM AN epc student,
-  
- -b) sentence which has correct syntax but has semantic errors is 
-
- I python love program.
