33e7478b22b01b63ab6a57f672e3dcf9efad0a81
BROWN-PROGRAMTECH <39649631+BROWN-PROGRAMTECH@users.noreply.github.com>
2018-07-20 19:41:11 +0100

    Delete exercise_1.12.1.py

--- Brown/chapter_1/exercise_1.12.1.py
+++
@@ -1,7 +0,0 @@
- 1-a) An English (or Dutch!) sentence with understandable semantics but incorrect syntax is
-  
- i AM AN epc student,
-  
- -b)English (or Dutch!) sentence which has correct syntax but has semantic errors is 
-
- I python love program.
